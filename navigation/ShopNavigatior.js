6a969bebe622a3c4adfc4c6f16730a50e2050273
Samuel Brehm <samuelbrehm@gmail.com>
2020-01-30 15:12:27 -0300

    Added navigation, styling and fonts for product detail screen

--- navigation/ShopNavigatior.js
+++ navigation/ShopNavigatior.js
@@ -3,17 +3,25 @@ import { createAppContainer } from "react-navigation";
 import { Platform } from "react-native";
 
 import ProductsOverviewScreen from "../screens/shop/ProductsOverviweScreen";
+import ProductDetailScreen from "../screens/shop/ProductDetailScreen";
 
 import Colors from "../constants/Colors";
 
 const ProductsNavigator = createStackNavigator(
   {
-    ProductsOverview: ProductsOverviewScreen
+    ProductsOverview: ProductsOverviewScreen,
+    ProductDetail: ProductDetailScreen
   },
   {
     defaultNavigationOptions: {
       headerStyle: {
         backgroundColor: Platform.OS === "android" ? Colors.primary : ""
+      },
+      headerTitleStyle: {
+        fontFamily: "open-sans-bold"
+      },
+      headerBackTitleStyle: {
+        fontFamily: "open-sans"
       },
       headerTintColor: Platform.OS === "android" ? "#ffffff" : Colors.primary
     }
