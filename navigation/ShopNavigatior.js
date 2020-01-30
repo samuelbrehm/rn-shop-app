83392d8c1d652977db9fc8d02e9f4d1a5c0ef9ff
Samuel Brehm <samuelbrehm@gmail.com>
2020-01-30 09:57:25 -0300

    Init page product ovweview

---
+++ navigation/ShopNavigatior.js
@@ -0,0 +1,23 @@
+import { createStackNavigator } from "react-navigation-stack";
+import { createAppContainer } from "react-navigation";
+import { Platform } from "react-native";
+
+import ProductsOverviewScreen from "../screens/shop/ProductsOverviweScreen";
+
+import Colors from "../constants/Colors";
+
+const ProductsNavigator = createStackNavigator(
+  {
+    ProductsOverview: ProductsOverviewScreen
+  },
+  {
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: Platform.OS === "android" ? Colors.primary : ""
+      },
+      headerTintColor: Platform.OS === "android" ? "#ffffff" : Colors.primary
+    }
+  }
+);
+
+export default createAppContainer(ProductsNavigator);
