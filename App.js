13b19cf8ed6603d450b6f4149b578e99ecf3a637
Samuel Brehm <samuelbrehm@gmail.com>
2020-01-31 17:22:21 -0300

    Build screens cart, orders and settings redux

--- App.js
+++ App.js
@@ -2,16 +2,23 @@ import React, { useState } from "react";
 import { createStore, combineReducers } from "redux";
 import { Provider } from "react-redux";
 
+// import { composeWithDevTools } from "redux-devtools-extension";
+
 import { AppLoading } from "expo";
 import * as Font from "expo-font";
 
 import productsReducer from "./store/reducers/products";
-import ShopNavigator from "./navigation/ShopNavigatior";
+import cartReducer from "./store/reducers/cart";
+import ordersReducer from "./store/reducers/orders";
+import ShopNavigator from "./navigation/ShopNavigator";
 
 const rootReducer = combineReducers({
-  products: productsReducer
+  products: productsReducer,
+  cart: cartReducer,
+  orders: ordersReducer
 });
 
+// const store = createStore(rootReducer, composeWithDevTools());
 const store = createStore(rootReducer);
 
 const fetchFonts = () => {
--- screens/shop/ProductDetailScreen.js
+++ screens/shop/ProductDetailScreen.js
@@ -7,21 +7,29 @@ import {
   Button,
   StyleSheet
 } from "react-native";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
 import Colors from "../../constants/Colors";
+import * as cartActions from "../../store/actions/cart";
 
 const ProductDetailScreen = props => {
   const productId = props.navigation.getParam("productId");
   const selectedProduct = useSelector(state =>
     state.products.availableProducts.find(prod => prod.id === productId)
   );
+  const dispatch = useDispatch();
 
   return (
     <ScrollView>
       <Image style={styles.image} source={{ uri: selectedProduct.imageUrl }} />
       <View style={styles.actions}>
-        <Button color={Colors.primary} title="Add toCart" onPress={() => {}} />
+        <Button
+          color={Colors.primary}
+          title="Add toCart"
+          onPress={() => {
+            dispatch(cartActions.addToCart(selectedProduct));
+          }}
+        />
       </View>
       <Text style={styles.price}>${selectedProduct.price}</Text>
       <Text style={styles.description}>{selectedProduct.description}</Text>
